518ed3e84a2e158ce85581b9b7265e97ccd43f92
YOzaz <yozaz.mandac@gmail.com>
2025-06-23 10:50:15 +0300

    cleanup: Remove redundant landing route and page
    - Removed /landing route from web.php
    - Deleted landing.blade.php file
    - Welcome page now serves as the main landing page

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@ Route::get('/about', function () {
     return view('about');
 })->name('about');
 
-Route::get('/landing', function () {
-    return view('landing');
-})->name('landing');
-
 Route::get('/features', function () {
     return view('features.index');
 })->name('features');
